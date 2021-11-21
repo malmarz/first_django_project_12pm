22121eaa42d920993b9546b1ee67b40a8f6de294
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-11-21 10:06:53 +0300

    end of class

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp.views import test_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test_view),
 ]
