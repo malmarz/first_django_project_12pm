eb453151476fa41efd41287269e8b9ce55a885d2
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-11-23 10:05:55 +0300

    progress 12 pm

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def test_view(request):
+    print("request:", dir(request))
+    return HttpResponse("""
+    <html>
+     <head></head>
+     <body>
+     Hello World
+     This is my first
+      HTML page
+     </body>
+    </html>
+    """)
